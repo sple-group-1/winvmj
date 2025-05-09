4c489220922a8d6e13cb6bf3f1e67f5ca66e1645
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-05-09 19:31:10 +0700

    fix: roomoption for hotel compilation

--- modules/OnlineTicketing.bookingoption.roomoption/OnlineTicketing/bookingoption/roomoption/resource/BookingOptionResourceImpl.java
+++ modules/OnlineTicketing.bookingoption.roomoption/OnlineTicketing/bookingoption/roomoption/resource/BookingOptionResourceImpl.java
@@ -1,100 +1,98 @@
 package OnlineTicketing.bookingoption.roomoption;
+
 import java.util.*;
 
 import vmj.routing.route.Route;
 import vmj.routing.route.VMJExchange;
+import vmj.routing.route.exceptions.*;
 
 import OnlineTicketing.bookingoption.core.BookingOptionResourceDecorator;
-import OnlineTicketing.bookingoption.core.BookingOptionImpl;
 import OnlineTicketing.bookingoption.core.BookingOptionResourceComponent;
+import OnlineTicketing.bookingoption.core.BookingOption;
+
+
+import OnlineTicketing.bookingoption.roomoption.BookingOptionImpl;
 
 public class BookingOptionResourceImpl extends BookingOptionResourceDecorator {
-    public BookingOptionResourceImpl (BookingOptionResourceComponent record) {
-        super(record);
-    }
 
-    // @Restriced(permission = "")
-    @Route(url="call/roomoption/save")
-    public List<HashMap<String,Object>> save(VMJExchange vmjExchange){
-		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
-			return null;
-		}
-		  = create(vmjExchange);
-		Repository.saveObject();
-		return getAll(vmjExchange);
-	}
+	private BookingOptionServiceImpl roomOptionService;
 
-    public BookingOption create(VMJExchange vmjExchange){
-		String roomType = (String) vmjExchange.getRequestBodyForm("roomType");
-		
-		  = record.create(vmjExchange);
-		 deco = Factory.create("OnlineTicketing.roomoption.core.BookingOptionImpl", , roomType);
-			return deco;
-	}
-
-    public BookingOption create(VMJExchange vmjExchange, int id){
-		String roomType = (String) vmjExchange.getRequestBodyForm("roomType");
-		  = Repository.getObject(id);
-		int recordId = (((Decorator) saved.getRecord()).getId();
-		
-		  = record.create(vmjExchange);
-		 deco = Factory.create("OnlineTicketing.roomoption.core.BookingOptionImpl", id, , roomType);
-			return deco;
-	}
-
-    // @Restriced(permission = "")
-    @Route(url="call/roomoption/update")
-    public HashMap<String, Object> update(VMJExchange vmjExchange){
-		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
-			return null;
-		}
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
-		int id = Integer.parseInt(idStr);
-		
-		  = Repository.getObject(id);
-		 = create(vmjExchange, id);
-		
-		Repository.updateObject();
-		 = Repository.getObject(id);
-		//to do: fix association attributes
-		
-		return .toHashMap();
-		
+	public BookingOptionResourceImpl(BookingOptionResourceComponent record) {
+		super(record);
+		this.roomOptionService = new BookingOptionServiceImpl(
+				new OnlineTicketing.bookingoption.core.BookingOptionServiceImpl());
 	}
 
 	// @Restriced(permission = "")
-    @Route(url="call/roomoption/detail")
-    public HashMap<String, Object> get(VMJExchange vmjExchange){
-		return record.getBookingOption(vmjExchange);
+	// @Route(url="call/roomoption/save")
+	// public List<HashMap<String,Object>> save(VMJExchange vmjExchange){
+	// if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
+	// return null;
+	// }
+	// = create(vmjExchange);
+	// Repository.saveObject();
+	// return getAll(vmjExchange);
+	// }
+
+	@Route(url = "call/roomoption/save")
+	public HashMap<String, Object> create(VMJExchange vmjExchange) {
+		if (vmjExchange.getHttpMethod().equals("POST")) {
+			Map<String, Object> requestBody = vmjExchange.getPayload();
+			BookingOption result = this.roomOptionService.createBookingOption(requestBody);
+			return result.toHashMap();
+		}
+		throw new NotFoundException("Route tidak ditemukan");
 	}
+
+	// public BookingOption create(VMJExchange vmjExchange, int id){
+	// String roomType = (String) vmjExchange.getRequestBodyForm("roomType");
+	// = Repository.getObject(id);
+	// int recordId = (((Decorator) saved.getRecord()).getId();
+
+	// = record.create(vmjExchange);
+	// deco = Factory.create("OnlineTicketing.roomoption.core.BookingOptionImpl",
+	// id, , roomType);
+	// return deco;
+	// }
 
 	// @Restriced(permission = "")
-    @Route(url="call/roomoption/list")
-    public List<HashMap<String,Object>> getAll(VMJExchange vmjExchange){
-		List<> List = Repository.getAllObject("_impl");
-		return transformListToHashMap(List);
-	}
-
-    public List<HashMap<String,Object>> transformListToHashMap(List<> List){
-		List<HashMap<String,Object>> resultList = new ArrayList<HashMap<String,Object>>();
-        for(int i = 0; i < List.size(); i++) {
-            resultList.add(List.get(i).toHashMap());
-        }
-
-        return resultList;
-	}
-
-	// @Restriced(permission = "")
-    @Route(url="call/roomoption/delete")
-    public List<HashMap<String,Object>> deleteBookingOption(VMJExchange vmjExchange){
+	@Route(url = "call/roomoption/update")
+	public HashMap<String, Object> update(VMJExchange vmjExchange) {
 		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
-		
-		String idStr = (String) vmjExchange.getRequestBodyForm("");
-		int id = Integer.parseInt(idStr);
-		Repository.deleteObject(id);
-		return getAll(vmjExchange);
+		return this.roomOptionService.updateBookingOption(vmjExchange.getPayload());
+	}
+
+	// @Restriced(permission = "")
+	@Route(url = "call/roomoption/detail")
+	public HashMap<String, Object> get(VMJExchange vmjExchange) {
+		return this.roomOptionService.getBookingOption(vmjExchange.getPayload());
+	}
+
+	// @Restriced(permission = "")
+	@Route(url = "call/roomoption/list")
+	public List<HashMap<String, Object>> getAll(VMJExchange vmjExchange) {
+		return this.roomOptionService.getAllBookingOption(vmjExchange.getPayload());
+	}
+
+	public List<HashMap<String, Object>> transformListToHashMap(List<BookingOptionImpl> List) {
+		List<HashMap<String, Object>> resultList = new ArrayList<HashMap<String, Object>>();
+		for (int i = 0; i < List.size(); i++) {
+			resultList.add(List.get(i).toHashMap());
+		}
+
+		return resultList;
+	}
+
+	// @Restriced(permission = "")
+	@Route(url = "call/roomoption/delete")
+	public List<HashMap<String, Object>> deleteBookingOption(VMJExchange vmjExchange) {
+		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
+			return null;
+		}
+
+		return this.roomOptionService.deleteBookingOption(vmjExchange.getPayload());
 	}
 
 }
