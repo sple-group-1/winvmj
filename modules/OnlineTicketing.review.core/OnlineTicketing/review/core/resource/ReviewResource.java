bfcce2e6237244f7303b65fc1deffafc5d494ff6
DarilRaf <darilmrafansyah@gmail.com>
2025-05-09 10:30:52 +0700

    fix: resource layer fixed

--- modules/OnlineTicketing.review.core/OnlineTicketing/review/core/resource/ReviewResource.java
+++ modules/OnlineTicketing.review.core/OnlineTicketing/review/core/resource/ReviewResource.java
@@ -1,13 +1,18 @@
 package OnlineTicketing.review.core;
+
 import java.util.*;
 
 import vmj.routing.route.VMJExchange;
 
 public interface ReviewResource {
-    List<HashMap<String,Object>> saveReview(VMJExchange vmjExchange);
+    // List<HashMap<String,Object>> saveReview(VMJExchange vmjExchange);
     HashMap<String, Object> updateReview(VMJExchange vmjExchange);
+
     HashMap<String, Object> getReview(VMJExchange vmjExchange);
-    List<HashMap<String,Object>> getAllReview(VMJExchange vmjExchange);
-    List<HashMap<String,Object>> deleteReview(VMJExchange vmjExchange);
-	HashMap<String, Object> createReview(VMJExchange vmjExhange);
+
+    List<HashMap<String, Object>> getAllReview(VMJExchange vmjExchange);
+
+    List<HashMap<String, Object>> deleteReview(VMJExchange vmjExchange);
+
+    HashMap<String, Object> createReview(VMJExchange vmjExhange);
 }
--- modules/OnlineTicketing.review.core/OnlineTicketing/review/core/resource/ReviewResourceImpl.java
+++ modules/OnlineTicketing.review.core/OnlineTicketing/review/core/resource/ReviewResourceImpl.java
@@ -1,4 +1,5 @@
 package OnlineTicketing.review.core;
+
 import java.util.*;
 
 import vmj.routing.route.Route;
@@ -8,55 +9,54 @@ import OnlineTicketing.review.ReviewFactory;
 //import prices.auth.vmj.annotations.Restricted;
 //add other required packages
 
-public class ReviewResourceImpl extends ReviewResourceComponent{
-	
+public class ReviewResourceImpl extends ReviewResourceComponent {
+
 	private ReviewServiceImpl reviewServiceImpl = new ReviewServiceImpl();
 
 	// @Restriced(permission = "")
-    @Route(url="call/review")
-    public HashMap<String,Object> createreview(VMJExchange vmjExchange){
+	@Route(url = "call/review")
+	public HashMap<String, Object> createReview(VMJExchange vmjExchange) {
 		if (vmjExchange.getHttpMethod().equals("POST")) {
-		    Map<String, Object> requestBody = vmjExchange.getPayload(); 
+			Map<String, Object> requestBody = vmjExchange.getPayload();
 			Review result = reviewServiceImpl.createReview(requestBody);
 			return result.toHashMap();
 		}
 		throw new NotFoundException("Route tidak ditemukan");
 	}
 
-    // @Restriced(permission = "")
-    @Route(url="call/review/update")
-    public HashMap<String, Object> updateReview(VMJExchange vmjExchange){
-		Map<String, Object> requestBody = vmjExchange.getPayload(); 
-		if (vmjExchange.getHttpMethod().equals("OPTIONS")){
+	// @Restriced(permission = "")
+	@Route(url = "call/review/update")
+	public HashMap<String, Object> updateReview(VMJExchange vmjExchange) {
+		Map<String, Object> requestBody = vmjExchange.getPayload();
+		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
 		return reviewServiceImpl.updateReview(requestBody);
-		
+
 	}
 
 	// @Restriced(permission = "")
-    @Route(url="call/review/detail")
-    public HashMap<String, Object> getReview(VMJExchange vmjExchange){
-		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+	@Route(url = "call/review/detail")
+	public HashMap<String, Object> getReview(VMJExchange vmjExchange) {
+		Map<String, Object> requestBody = vmjExchange.getPayload();
 		return reviewServiceImpl.getReview(requestBody);
 	}
 
 	// @Restriced(permission = "")
-    @Route(url="call/review/list")
-    public List<HashMap<String,Object>> getAllReview(VMJExchange vmjExchange){
-		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+	@Route(url = "call/review/list")
+	public List<HashMap<String, Object>> getAllReview(VMJExchange vmjExchange) {
+		Map<String, Object> requestBody = vmjExchange.getPayload();
 		return reviewServiceImpl.getAllReview(requestBody);
 	}
 
-    
 	// @Restriced(permission = "")
-    @Route(url="call/review/delete")
-    public List<HashMap<String,Object>> deleteReview(VMJExchange vmjExchange){
-		Map<String, Object> requestBody = vmjExchange.getPayload(); 
+	@Route(url = "call/review/delete")
+	public List<HashMap<String, Object>> deleteReview(VMJExchange vmjExchange) {
+		Map<String, Object> requestBody = vmjExchange.getPayload();
 		if (vmjExchange.getHttpMethod().equals("OPTIONS")) {
 			return null;
 		}
-		
+
 		return reviewServiceImpl.deleteReview(requestBody);
 	}
 
