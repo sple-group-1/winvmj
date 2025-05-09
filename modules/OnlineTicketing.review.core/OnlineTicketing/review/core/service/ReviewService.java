eec6b0249025806c4c3ddbfa5ffd8339b4ea4725
DarilRaf <darilmrafansyah@gmail.com>
2025-05-09 15:32:04 +0700

    fix: service layer fixed and successfully compiled

--- modules/OnlineTicketing.review.core/OnlineTicketing/review/core/service/ReviewService.java
+++ modules/OnlineTicketing.review.core/OnlineTicketing/review/core/service/ReviewService.java
@@ -5,12 +5,12 @@ import vmj.routing.route.VMJExchange;
 
 public interface ReviewService {
 	Review createReview(Map<String, Object> requestBody);
-	Review createReview(Map<String, Object> requestBody, Map<String, Object> response);
+	// Review createReview(Map<String, Object> requestBody, Map<String, Object> response);
 	HashMap<String, Object> getReview(Map<String, Object> requestBody);
-    List<HashMap<String,Object>> saveReview(Map<String, Object> requestBody);
+    // List<HashMap<String,Object>> saveReview(Map<String, Object> requestBody);
     HashMap<String, Object> updateReview(Map<String, Object> requestBody);
     HashMap<String, Object> getReviewById(int id);
     List<HashMap<String,Object>> getAllReview(Map<String, Object> requestBody);
     List<HashMap<String,Object>> deleteReview(Map<String, Object> requestBody);
-	List<HashMap<String, Object>> transformListToHashMap(List<Disbursement> List);
+	// List<HashMap<String, Object>> transformListToHashMap(List<Disbursement> List);
 }
--- modules/OnlineTicketing.review.core/OnlineTicketing/review/core/service/ReviewServiceComponent.java
+++ modules/OnlineTicketing.review.core/OnlineTicketing/review/core/service/ReviewServiceComponent.java
@@ -6,15 +6,15 @@ import vmj.routing.route.VMJExchange;
 //add other required packages
 
 public abstract class ReviewServiceComponent implements ReviewService{
-	protected RepositoryUtil<Review> Repository;
+	protected RepositoryUtil<Review> reviewRepository;
 
     public ReviewServiceComponent(){
-        this.Repository = new RepositoryUtil<Review>(OnlineTicketing.review.core.ReviewComponent.class);
+        this.reviewRepository = new RepositoryUtil<Review>(OnlineTicketing.review.core.ReviewComponent.class);
     }	
 
-    public abstract List<HashMap<String,Object>> saveReview(VMJExchange vmjExchange);
-    public abstract Review createReview(Map<String, Object> requestBodye);
-	public abstract Review createReview(Map<String, Object> requestBody, Map<String, Object> response);    
+    // public abstract List<HashMap<String,Object>> saveReview(VMJExchange vmjExchange);
+    public abstract Review createReview(Map<String, Object> requestBody);
+	// public abstract Review createReview(Map<String, Object> requestBody, Map<String, Object> response);    
 	public abstract HashMap<String, Object> updateReview(Map<String, Object> requestBody);
     public abstract HashMap<String, Object> getReview(Map<String, Object> requestBody);
     public abstract List<HashMap<String,Object>> getAllReview(Map<String, Object> requestBody);
