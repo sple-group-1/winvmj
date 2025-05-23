17b319d1e9ceab98c4f9ca086ccfd0f0ee65ab5e
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-05-23 19:48:49 +0700

    fix: booking availability

--- modules/OnlineTicketing.bookingavailability.core/OnlineTicketing/bookingavailability/core/service/BookingAvailabilityServiceDecorator.java
+++ modules/OnlineTicketing.bookingavailability.core/OnlineTicketing/bookingavailability/core/service/BookingAvailabilityServiceDecorator.java
@@ -16,30 +16,27 @@ public abstract class BookingAvailabilityServiceDecorator extends BookingAvailab
 	}
 
 	
-	public HashMap<String, Object> getBookingAvailability(Map<String, Object> requestBody) {
+	public BookingAvailability getBookingAvailability(Map<String, Object> requestBody) {
 		return record.getBookingAvailability(requestBody);
 	}
 
-	public List<HashMap<String, Object>> getAllBookingAvailability(Map<String, Object> requestBody) {
+	public List<BookingAvailability> getAllBookingAvailability(Map<String, Object> requestBody) {
 		return record.getAllBookingAvailability(requestBody);
 	}
 
 
-
-	public HashMap<String, Object> updateBookingAvailability(Map<String, Object> requestBody) {
+	public BookingAvailability updateBookingAvailability(Map<String, Object> requestBody) {
 		return record.updateBookingAvailability(requestBody);
 	}
 
-	public List<HashMap<String, Object>> transformListToHashMap(List<BookingAvailability> List) {
-		return record.transformListToHashMap(List);
-	}
-
-	public List<HashMap<String, Object>> deleteBookingAvailability(Map<String, Object> requestBody) {
+	
+	public List<BookingAvailability> deleteBookingAvailability(Map<String, Object> requestBody) {
 		return record.deleteBookingAvailability(requestBody);
 	}
-
-	public HashMap<String, Object> getBookingAvailabilityById(UUID id) {
-		return record.getBookingAvailabilityById(id);
+	
+	
+	public List<HashMap<String, Object>> transformListToHashMap(List<BookingAvailability> List) {
+		return record.transformListToHashMap(List);
 	}
 
 }
